cb879e3f270304d24ca83555ced0ac22ea47fed2
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-28 23:42:00 +0700

    setup: custom config

---
+++ config/config.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const {
+  DB_CONNECTION,
+  DB_HOST,
+  DB_DATABASE,
+  DB_USERNAME,
+  DB_PASSWORD,
+} = process.env;
+
+module.exports = {
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: DB_CONNECTION,
+  },
+  test: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: DB_CONNECTION,
+  },
+  production: {
+    username: DB_USERNAME,
+    password: null,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: DB_CONNECTION,
+  },
+};
