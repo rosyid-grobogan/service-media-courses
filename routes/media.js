ee9807866ee65d6b77950f477a8b9de8134c0677
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-29 20:12:07 +0700

    API DELETE

--- routes/media.js
+++ routes/media.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const isBase64 = require('is-base64');
 const base64Img = require('base64-img');
+const fs = require('fs');
 
 const { Media } = require('../models');
 
@@ -44,6 +45,32 @@ router.post('/', (req, res) => {
         id: media.id,
         image: `${req.get('host')}/images/${filename}`,
       },
+    });
+  });
+});
+
+// DELETE
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  const media = await Media.findByPk(id);
+
+  if (!media) {
+    return res
+      .status(404)
+      .json({ status: 'error', message: 'media not found' });
+  }
+
+  fs.unlink(`./public/${media.image}`, async (err) => {
+    if (err) {
+      return res.status(400).json({ status: 'error', message: err.message });
+    }
+
+    await media.destroy();
+
+    return res.json({
+      status: 'success',
+      message: 'image deleted',
     });
   });
 });
